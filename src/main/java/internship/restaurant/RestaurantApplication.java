6372aab3f8deca1f0d185588daf9ba6156302814
Dylaren487 <Dylaren_487@hotmail.com>
2017-04-09 20:23:15 +0700

    War export dependency added

--- src/main/java/internship/restaurant/RestaurantApplication.java
+++ src/main/java/internship/restaurant/RestaurantApplication.java
@@ -2,9 +2,16 @@ package internship.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestaurantApplication {
+public class RestaurantApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestaurantApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
